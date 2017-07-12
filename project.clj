f58c9630dbf79ab3883bf49b3c0b96e272a92e90
dptd <dptdescribe@gmail.com>
2017-07-12 22:29:41 +0200

    add http client

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :description "Just Another Discogs Wrapper"
   :url "https://github.com/VioletMustache/jadw"
   :license {:name "MIT"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [clj-http "3.6.1"]]
   :main ^:skip-aot jadw.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/jadw/core.clj
+++ src/jadw/core.clj
@@ -1,7 +1,21 @@
 (ns jadw.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clj-http.client :as client]))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(def *token*
+  "Discogs API token."
+  nil)
+
+(defn set-token
+  "Set Discogs API token."
+  [token]
+  (def *token* token))
+
+(def *discogs-api*
+  "Discogs API URL."
+  "https://api.discogs.com/database/")
